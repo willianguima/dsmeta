66ae8ac0c50ed4c412c374cd085d24b22575da8d
willian <willianguimaraes98@gmail.com>
2022-11-15 21:12:35 -0300

    fist component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
-
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
-    <h1>ola mundo</h1>
+    <>
+      <h1>olá mundo222</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      
+    </>
   )
 }
 
