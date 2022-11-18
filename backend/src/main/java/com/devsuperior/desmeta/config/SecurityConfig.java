d764cb3a68429e981e450f99ecfc56749c7881ea
willian <willianguimaraes98@gmail.com>
2022-11-17 21:46:24 -0300

    sql

--- backend/src/main/java/com/devsuperior/desmeta/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/desmeta/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.devsuperior.desmeta.config;
 
 import java.util.Arrays;
 
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
